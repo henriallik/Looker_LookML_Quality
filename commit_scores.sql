cbdca23040cde2bec34c8bbc18f57ad123db4fdd
Henri Allik <henri@transferwise.com>
2018-09-28 12:12:12 +0300

    Added few comments

--- commit_scores.sql
+++ commit_scores.sql
@@ -36,8 +36,9 @@ group by 1,2,3,4,5,6,7,8,9,10
 having (SELECT count(*) from regexp_matches(c.lines_removed,p.keyword,'g')) > 0 or (SELECT count(*) from regexp_matches(c.lines_added,p.keyword,'g')) > 0 -- cartesian join so this will kick out the dummies
 ;
 
-
+-- Removing commits by Segah - he was developer from Looker who helped us to set up LookML models
 UPDATE reports.lookup_lookml_quality_commits_combined set author= null where author = 'Segah Meer';
+-- Deleting commits from 10.04.2017 since we upgraded LookML version                                                                                                                      
 DELETE FROM reports.lookup_lookml_quality_commits_combined where commit_time = '2017-04-10';
 
 CREATE INDEX IF NOT EXISTS ix1_lookup_lookml_quality_commits_combined on reports.lookup_lookml_quality_commits_combined(model_name);
